430a1a2ea3481c24ccb76c6dc57086c3fb4b0966
Anna N Helms <unicornbuddha@gmail.com>
2025-01-26 15:20:09 -0800

    adds axios, get, and post requests to content. creates PhotoIndex and PhotoNew

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { PhotosIndex } from "./PhotosIndex";
+import { PhotosNew } from "./PhotosNew";
+
 export function Content() {
+    const [photos, setPhotos] = useState([]);
+
+    const handleIndexPhotos = () => {
+        console.log("handleIndexPhotos");
+        axios.get("http://localhost:3000/photos.json").then((response) => {
+            console.log(response.data);
+            setPhotos(response.data);
+        });
+    };
+
+    const handleCreatePhoto = (params, successCallback) => {
+        console.log("handleCreatePhoto", params);
+        axios.post("http://localhost:3000/photos.json", params).then((response) => {
+            setPhotos([...photos, response.data]);
+            successCallback();
+        });
+    };
+
+    useEffect(handleIndexPhotos, []);
+
     return (
         <main>
-            <h1>Welcome to React!</h1>
+            <PhotosNew onCreatePhoto={handleCreatePhoto} />
+            <PhotosIndex photos={photos} />
         </main>
-    )
+    );
 }
